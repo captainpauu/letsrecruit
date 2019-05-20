69a500ed58baf31d7d11e32c5b0e7f9fd157c3d7
Pravin Dhange <pravin.dhange@aventri.com>
2019-05-20 15:53:05 +0530

    PDO connection started

--- config/routes.php
+++ config/routes.php
@@ -1,38 +1,8 @@
 <?php
 
-use Slim\Http\Request;
-use Slim\Http\Response;
-use App\Services\ServiceProvider;
-use App\Controllers\CandidateController;
-
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("It works! This is the default welcome page.");
-    return $response;
-})->setName('root');
-
-$app->get('/candidateDashboard', function (Request $request, Response $response, $args) {
-
-    return $this->smarty->render($response, 'candidateDashboard.tpl');
-})->setName('header');
-
-$app->get('/login', function (Request $request, Response $response, $args) {
-
-    return $this->smarty->render($response, 'login.tpl');
-})->setName('login');
-
-$app->get('/addCandidate', function (Request $request, Response $response, $args) {
-
-    return $this->smarty->render($response, 'addCandidate.tpl');
-})->setName('addCandidate');
-
-$app->get('/candidateProfile', function (Request $request, Response $response, $args) {
-
-    return $this->smarty->render($response, 'candidateProfile.tpl');
-})->setName('candidateProfile');
-
 $app->group('/candidate',
             function () use ($app) {
-    $app->get('/profile', 'CandidateController:getAllCandidates');
+    $app->get('/dashboard', 'CandidateController:getAllCandidates');
 
     $app->get('/add', 'CandidateController:addCandidate');
 });
--- src/Controllers/CandidateController.php
+++ src/Controllers/CandidateController.php
@@ -3,23 +3,26 @@
 
 namespace App\Controllers;
 
+use App\Dao\CandidateDao;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 class CandidateController extends BaseController
 {
-    public function __construct($smarty)
+    protected $dao;
+
+    public function __construct($smarty, CandidateDao $dao)
     {
         parent::__construct($smarty);
+        $this->dao = $dao;
     }
 
-    public function getAllCandidates(RequestInterface $request, ResponseInterface $response, $args)
+    public function getAllCandidates(RequestInterface $request, ResponseInterface $response)
     {
-        return $this->smarty->render($response, 'candidateProfile.tpl');
-    }
-
-    public function addCandidate(RequestInterface $request, ResponseInterface $response, $args)
-    {
-        return $this->smarty->render($response, 'addCandidate.tpl');
+        $allCandidates = $this->dao->getAllCandidates();
+        return $this->smarty->render(
+            $response,
+            'candidateDashboard.tpl',
+            ['allCandidates' => $allCandidates]);
     }
 }
